454472447195a1e132534a38a5e149857eb78e3a
Julia Marushchenko <marushchenko.yulia@gmail.com>
2025-05-23 00:30:54 +0200

    Add files via upload
    Committing Main class

---
+++ src/com/unic/Main.java
@@ -0,0 +1,30 @@
+// Java program to find unic element
+package com.unic;
+
+import java.util.HashSet;
+import java.util.Set;
+
+// Main class
+public class Main {
+
+    // Method findUnicNumber to find number is unic
+    public static int findUnicNumber(int[] arr) {
+
+        // Using set
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (set.contains(arr[i])) {
+                set.remove(arr[i]);
+            } else {
+                set.add(arr[i]);
+            }
+        }
+        return (int) set.toArray()[0];
+    }
+
+        public static void main (String [] args){
+
+            int [] array = {3, 5, 4, 5, 6, 6, 8, 3, 4};
+            System.out.println(findUnicNumber(array));
+    }
+}
